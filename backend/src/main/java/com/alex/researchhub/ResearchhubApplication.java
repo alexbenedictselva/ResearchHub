ca29d36e64d163df61837b90cdf5f7a12055bdfe
Alex Benedict Selva <alexalex1772006@gmail.com>
2026-07-15 21:23:38 +0530

    Created the maven project

---
+++ backend/src/main/java/com/alex/researchhub/ResearchhubApplication.java
@@ -0,0 +1,13 @@
+package com.alex.researchhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchhubApplication.class, args);
+	}
+
+}
